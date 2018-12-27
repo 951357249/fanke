f9440b686abded434b8ff91c7edf5cf44f1b5b2c
ww <951357249@qq.com>
2018-12-27 22:48:57 +0800

    js

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 
 gulp.task('js',function(){
-	gulp.src('./src/js/*js').pipe(concat('index.min,js')).pipe(gulp-dest('./dist'));
+	gulp.src('./src/js/*/*js').pipe(concat('index.min.js')).pipe(uglify()).pipe(gulp.dest('./dist/js'));
 })
 gulp.task('sass',function(){
-	gulp.src('./src/sass/*.scss').pipe(sass()).pipe(cssnano()).pipe(gulp-dest('./dist'));
+	gulp.src('./src/sass/*.scss').pipe(sass()).pipe(cssnano()).pipe(gulp.dest('./dist/css'));
 })
 gulp.task('default',function(){
-	gulp.watch('./src/js/*.js',['js']);
+	gulp.watch('./src/js/*/*.js',['js']);
 	gulp.watch('./src/sass/*.scss',['sass']);
 })
