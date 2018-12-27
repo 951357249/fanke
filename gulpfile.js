7013c25bc8c7e74c4f52ec0aea5b064e50336cb0
ww <951357249@qq.com>
2018-12-27 18:15:43 +0800

    凡客诚品

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+
+gulp.task('js',function(){
+	gulp.src('./src/js/*js').pipe(concat('index.min,js')).pipe(gulp-dest('./dist'));
+})
+gulp.task('sass',function(){
+	gulp.src('./src/sass/*.scss').pipe(sass()).pipe(cssnano()).pipe(gulp-dest('./dist'));
+})
+gulp.task('default',function(){
+	gulp.watch('./src/js/*.js',['js']);
+	gulp.watch('./src/sass/*.scss',['sass']);
+})
